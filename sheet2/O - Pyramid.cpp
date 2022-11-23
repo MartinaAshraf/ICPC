dcf76c6193bc648557621e757cd74463ece4fd9b
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-11-23 19:39:44 +0200

    Create O - Pyramid.cpp

---
+++ sheet2/O - Pyramid.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <cstring>
+#include <vector>
+ 
+using namespace std;
+int main() {
+	int n , counter = 1;
+	cin >> n; 
+for (int i = counter; i <=n; i++)
+ 
+{
+for (int j = counter; j <= i; j++)
+ 
+	{
+ 
+		cout << "*";
+ 
+	}
+ 
+	cout << endl;
+ 
+}
+ 
+}

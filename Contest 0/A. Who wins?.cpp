005eac7c45bb35da2cb38e787f9b22cfc293380d
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-25 21:02:24 +0200

    Create A. Who wins?.cpp

---
+++ Contest 0/A. Who wins?.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    long long a,b;
+    cin>>a>>b;
+    if (a>b)
+    {
+      cout<<"Omar\n";
+    }
+    else if (a<b)
+    {
+      cout<<"Anas\n";
+    }
+    else
+    {
+      cout<<"Tie\n";
+    }
+ return 0;
+}

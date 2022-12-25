a5ae8ae1a7f2316a78551721026ad68b6dbbd473
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-25 21:11:31 +0200

    Create D - Cats and Dogs.cpp

---
+++ Contest 0/D - Cats and Dogs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b,x;
+  cin >>a >> b>> x;
+ if(a>x||x>(a+b))
+ {
+   cout<<"NO";
+ }
+  else 
+  {
+    cout<<"YES";
+  }
+return 0;
+    }

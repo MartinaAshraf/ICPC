a515e839e4affb10aca695071dfedf5835e7ed71
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-26 01:13:18 +0200

    Create B - Exam.cpp

---
+++ Contest 1/B - Exam.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >>n>>m;
+    if (n >= m) { cout << "YES" << endl; }
+    else { cout << "NO" << endl; }
+ 
+}

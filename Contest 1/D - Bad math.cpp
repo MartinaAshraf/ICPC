0fa7c6363041ed479ed1d8ed2d112333be1ef26c
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-26 01:15:34 +0200

    Create D - Bad math.cpp

---
+++ Contest 1/D - Bad math.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    fast();
+ 
+    long long num, t;
+    cin >> num >> t;
+ 
+    while (t--)
+    {
+        int digit = num % 10;
+ 
+        if (digit == 0)
+            num /= 10;
+        else
+            num--;
+    }
+    cout << num;
+}

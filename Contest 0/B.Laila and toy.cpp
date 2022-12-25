93975ef71fea40efe248a4937415dd2e5dcef54f
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-25 21:04:47 +0200

    Create B.Laila and toy.cpp

---
+++ Contest 0/B.Laila and toy.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    float a,b,c;
+    cin >> a>>b>>c;
+    if (a+b>=c)
+    {
+      cout<<"Yes\n";
+    }
+    else if (a+b<c)
+    {
+      cout<<"No\n";
+    }
+  
+}

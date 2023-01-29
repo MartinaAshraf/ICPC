368cd569fe0a89a415aef57f0109f37e217b66d2
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-01-29 18:38:05 +0200

    Create B - Basic Data Types.cpp

---
+++ sheet1/B - Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+int x;
+long long y;
+char z;
+float a;
+double b;
+    cin>> x>>y>>z>>a>>b ;
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+    cout << a << endl;
+    cout << b << endl;
+    return 0;
+}

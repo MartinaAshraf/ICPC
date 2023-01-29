f68589835d1ebfd44ac306899aa1a780da8920dc
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-01-29 18:39:48 +0200

    Create C - Simple Calculator.cpp

---
+++ sheet1/C - Simple Calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+ 
+long long x,y,a,c, b;
+    cin>> x>>y;
+    a=x+y;
+   b=x*y;
+   c=x-y;
+    cout << x<<" + "<<y << " = "<<a<< endl;
+    cout << x<<" * "<<y << " = "<<b<< endl;
+    cout << x<<" - "<<y << " = "<<c<<endl;
+ 
+    return 0;
+}

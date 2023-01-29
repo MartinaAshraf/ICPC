a4fb484cf23d57a396e9942d21237a89c416970a
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-01-29 18:47:42 +0200

    Create L - The Brothers.cpp

---
+++ sheet1/L - The Brothers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+string a,b,c,d;
+cin>>a>>b;
+cin>>c>>d;
+if (b==d){cout<<"ARE Brothers\n";}
+else{cout<<"NOT";}
+return 0;
+}

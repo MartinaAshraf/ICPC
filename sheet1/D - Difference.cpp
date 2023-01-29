7f963065d1325b941180e1d2a19321b39d52e612
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-01-29 18:40:54 +0200

    Create D - Difference.cpp

---
+++ sheet1/D - Difference.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+ 
+long long A,B,C,D,X;
+    cin>> A>>B>>C>>D;
+    X=(A*B)-(C*D);
+    cout << "Difference = "<<X<< endl;
+ 
+    return 0;
+}

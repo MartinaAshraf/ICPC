50400dc948d0cbaadb9204794c9d439cb6562d52
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2024-02-04 06:15:26 +0200

    H - Two numbers.cpp

--- sheet1/H - Two numbers.cpp
+++ sheet1/H - Two numbers.cpp
@@ -10,3 +10,31 @@ cout << "ceil "<<a<<" / "<<b<<" = "<<ceil(c)<<endl;
 cout << "round "<<a<<" / "<<b<<" = "<<round(c)<<endl;
 return 0;
 }
+
+
+//sel2
+#include <iostream>
+#include <math.h>
+#include<iomanip>
+using namespace std;
+int main() {
+  double A, B ;
+  int Floor;
+ double Ceil ,Round;
+    cin>>A>>B;
+   
+    if (A!=B){
+         Floor =A/B;
+     Ceil =Floor +1;
+     int m= (A/B)*10;
+     if(m%10>=5) 
+     Round = Ceil;
+     else 
+      Round =Floor;
+    }
+    else Floor = Ceil =Round =1;
+cout<<"floor "<<A<<" / "<<B<<" = "<<Floor<<endl<<
+"ceil "<<A<<" / "<<B<<" = "<<Ceil<<endl<<
+"round "<<A<<" / "<<B<<" = "<<Round;
+    return 0;
+}

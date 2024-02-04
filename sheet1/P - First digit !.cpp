b4b232e271ac5b5b1121f2bac016ca190b66041b
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2024-02-04 07:11:43 +0200

    P - First digit !.cpp

--- sheet1/P - First digit !.cpp
+++ sheet1/P - First digit !.cpp
@@ -1,14 +1,3 @@
-#include<iostream>
-#include<math.h>
-using namespace std;
-int main(){
-    int x,y;
-    cin>> x;
-    y=x/1000;
-    if (y%2==0){cout << "EVEN";}
-    else if (y%2!=0){cout << "ODD";}
-    return 0;}
-
 
 #include <iostream>
 #include <math.h>

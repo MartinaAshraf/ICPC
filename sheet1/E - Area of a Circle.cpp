c313836b87e53092b55ab85546ae012d2ca821a7
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2024-02-04 05:00:46 +0200

    E - Area of a Circle.cpp

--- sheet1/E - Area of a Circle.cpp
+++ sheet1/E - Area of a Circle.cpp
@@ -1,14 +1,11 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 #include<iomanip>
 using namespace std;
-int main(){
- 
-double R,Area;
-    cin>> R;
-    Area=3.141592653*R*R;
-    cout << fixed << setprecision(9);
-    cout <<Area<< endl;
- 
+int main() {
+    double R , Area;
+    cin>>R;
+    Area = 3.141592653*pow(R,2);
+  cout << setprecision(11)<<Area;
     return 0;
 }

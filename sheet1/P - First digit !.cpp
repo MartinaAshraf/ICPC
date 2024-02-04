0c44e2533cc4a92e11eca5d11d0d95ce2c409896
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2024-02-04 07:10:41 +0200

    P - First digit !.cpp

--- sheet1/P - First digit !.cpp
+++ sheet1/P - First digit !.cpp
@@ -8,3 +8,25 @@ int main(){
     if (y%2==0){cout << "EVEN";}
     else if (y%2!=0){cout << "ODD";}
     return 0;}
+
+
+#include <iostream>
+#include <math.h>
+#include<iomanip>
+#include <string>
+using namespace std;
+int main() {
+int a,b;
+cin >>a;
+if( a<10 &&a%2==0){cout <<"EVEN";}
+else if ( a<10 &&a%2!=0){cout <<"ODD";}
+else if ( a<100 &&(a/10)%2==0){cout <<"EVEN";}
+else if ( a<100 &&(a/10)%2!=0){cout <<"ODD";}
+else if ( a<1000 &&(a/100)%2==0){cout <<"EVEN";}
+else if (a<1000 &&(a/100)%2!=0){cout <<"ODD";}
+
+else if ((a/1000)%2==0){cout <<"EVEN";}
+else if ((a/1000)%2!=0){cout <<"ODD";}
+
+    return 0;
+}
